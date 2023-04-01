51d1c3061f7276126290ab88bc02e4b0f2f3d01b
Martin Becchio <tincho.becchio@gmail.com>
2023-04-01 20:31:52 -0300

    finalizando el hook de useMoneda

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,24 @@
 import { useState } from "react";
+import styled from "@emotion/styled";
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+`
 
 
 const useMoneda = (label, stateInicial, opciones) => {
@@ -8,8 +28,8 @@ const useMoneda = (label, stateInicial, opciones) => {
 
     const Seleccionar = () => (
         <>
-            <label>{label}</label>
-            <select
+            <Label>{label}</Label>
+            <Select
                 onChange={e => actualizarState(e.target.value)}
                 value={state}
             >
@@ -18,12 +38,12 @@ const useMoneda = (label, stateInicial, opciones) => {
                     <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
                 ))}
                 
-            </select>
+            </Select>
         </>
     )
     
     // Retornar state, interfaz y funcion que modifica el state
-    return [state, Seleccionar, actualizarState]
+    return [state, Seleccionar]
 
 }
  
