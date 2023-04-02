4862468907fb3ddc3fae7443888881b626a0fbef
Martin Becchio <tincho.becchio@gmail.com>
2023-04-01 21:17:42 -0300

    manejando el submit

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -26,7 +26,8 @@ const Boton = styled.input`
 const Formulario = () => {
 
     // state del listado de criptomonedas
-    const [listacripto, guardarCriptomonedas] = useState([])
+    const [ listacripto, guardarCriptomonedas] = useState([])
+    const [ error, guardarError ] = useState(false)
 
     const MONEDAS = [
         { codigo: 'USD', nombre: 'Dolar de Estados Unidos'},
@@ -53,9 +54,27 @@ const Formulario = () => {
         consultarAPI()
     },[])
 
+    // Cuando el usuario hace submit
+    const cotizarMoneda = e => {
+        e.preventDefault()
+
+        // validar si ambos campos estan llenos
+        if(moneda === '' || criptomoneda === '') {
+            guardarError(true)
+            return
+        }
+        // Paar los datos al componente principal
+        guardarError(false)
+
+
+
+    }
 
     return (
-        <form>
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            {error ? 'Hay un error' : null}
             <SelectMonedas />
 
             <SelectCripto />
